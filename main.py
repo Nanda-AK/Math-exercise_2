2740fed0e49263f9778890b602b7979aa3230672
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-03-16 06:54:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,10 +58,7 @@ Find the least common multiple (LCM) of 12 and 18.
 Questions End:
 
 choice Begin:
-["a": 6, 
-"b": 36, 
-"c": 72,
-"d": 216]
+[('a', 6), ('b', 36), ('c', 72), ('d', 216)]
 choice End:
 
 Answer Begin:
@@ -88,10 +85,7 @@ Sarah bought a bicycle that was originally priced at $250. She received a 15% di
 Questions End:
 
 choice Begin:
-["a": 187.50,
-"b": 186.50,
-"c": 37.50,
-"d": 287.50]
+[('a', 186.50), ('b', 184), ('c', 37.50), ('d', 216)]
 choice End:
 
 Answer Begin: a) $187.50 Answer End:
