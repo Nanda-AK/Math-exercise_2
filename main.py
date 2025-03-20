5c6e15d8b2371c55c01d8cc917375915d7a20f23
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-03-20 21:50:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ if st.session_state.llm_response:
         "Select one option:", options, index=None, key="answer_radio"
     )
     
-st.write(structured_llm)
+st.write(llm_response)
 
 if st.button("Submit Answer"):
     required_keys = ["Question", "A", "B", "C", "D", "Correct_Ans", "Explanation"]
