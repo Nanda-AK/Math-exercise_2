991f150551fad4ec7c5df3c3a3afd6f0f4cd9afa
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-04-01 12:23:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,10 +41,11 @@ if st.button(f"Generate {Math_topic} Math Problem"):
     #st.write(type(st.session_state.llm_response.content))
     #st.session_state.response_dict = st.session_state.llm_response.content
     st.session_state.response_dict = json.loads(st.session_state.llm_response.content)
-
-#options = ["Python", "Java", "C++", "JavaScript"]
-options = [st.session_state.response_dict["Choices"]["A"], st.session_state.response_dict["Choices"]["D"], st.session_state.response_dict["Choices"]["C"], st.session_state.response_dict["Choices"]["D"]]
-choice = st.radio("Select an option:", options)
+    
+    st.write(st.session_state.response_dict["Question"])
+    if st.button(f"Generate {Math_topic} Math Problem"):
+        options = [st.session_state.response_dict["Choices"]["A"], st.session_state.response_dict["Choices"]["D"], st.session_state.response_dict["Choices"]["C"], st.session_state.response_dict["Choices"]["D"]]
+        choice = st.radio("Select an option:", options)
     
 # Display user selection
 st.write(f"✅ You selected: **{choice}**")
